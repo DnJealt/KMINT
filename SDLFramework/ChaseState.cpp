6f94340c284223a0ddd4621de97eb4cbb4475bcf
Rick van Osta <osta1995@hotmail.com>
2017-03-28 12:48:18 +0200

    fixed chase

--- SDLFramework/ChaseState.cpp
+++ SDLFramework/ChaseState.cpp
@@ -9,11 +9,10 @@ ChaseState::~ChaseState() {}
 void ChaseState::Move(int totalTime) {
 	Vertex* pacmanVertex = ghost->getGame()->getPacman()->getNode();
 
-	if (pacmanVertex != ghost->getNode()) {
 		if (ghost->DistanceTo(ghost->getNode()->getX(), ghost->getNode()->getY()) < ghost->getSpeed() * 2) {
 			std::vector<Vertex*> path = AStar::find(ghost->getNode(), pacmanVertex, &ghost->getGame()->getMap());
 
-			ghost->setNode(path[0]);
+			if (!path.empty()) { ghost->setNode(path[0]); }
 		}
 		else {
 			if (ghost->getX() < ghost->getNode()->getX()) {
@@ -32,6 +31,4 @@ void ChaseState::Move(int totalTime) {
 
 		// Move the ghost to its new position
 		ghost->SetOffset(ghost->getX(), ghost->getY());
-	}
-
 }
